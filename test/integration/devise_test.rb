f015091397e9223381af9ab8f649c936fc4023c7
Trevor Turk <trevorturk@gmail.com>
2010-06-15 16:04:47 -0500

    tests for custom devise views

---
+++ test/integration/devise_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class DeviseTest < ActionDispatch::IntegrationTest
+
+  test "custom views" do
+    get new_user_session_path
+    assert_response :success
+    get new_user_password_path
+    assert_response :success
+    get new_user_registration_path
+    assert_response :success
+    get new_user_unlock_path
+    assert_response :success
+    get edit_user_password_path
+    assert_response :success
+    open_session do |s|
+      u = User.make(:password => 'test', :password_confirmation => 'test')
+      s.post user_session_path, :email => u.email, :password => 'test'
+      s.get destroy_user_session_path
+      assert_response :success
+    end
+    open_session do |s|
+      u = User.make(:password => 'test', :password_confirmation => 'test')
+      s.post user_session_path, :email => u.email, :password => 'test'
+      s.get edit_user_registration_path
+      assert_response :success
+    end
+  end
+
+end
