582a282e3efc93bb76b6fe34c8eb095cde6f3f31
Trevor Turk <trevorturk@gmail.com>
2010-09-06 10:56:38 -0500

    videos/show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,6 +7,7 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find params[:id]
+    @posts = @video.posts :order => 'posts.created_at', :include => :user
   end
 
 end
