1394d6072e521891ddcba59dd34b0527f5729e06
Trevor Turk <trevorturk@gmail.com>
2012-09-26 01:29:29 -0500

    fix people pages

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,6 +1,6 @@
 class SubscribersController < ApplicationController
   def create
-    @subscriber = Subscriber.create(params[:subscriber])
+    Subscriber.create(params[:subscriber])
     redirect_to(root_path, :notice => 'Thanks for signing up!')
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,5 @@
 class VideosController < ApplicationController
   def index
     @videos = Video.page(params[:page]).per_page(3)
-    @subscriber = Subscriber.new
   end
 end
