5ed8aa1419b482a8427b535de86f190b0d966d9a
Trevor Turk <trevorturk@gmail.com>
2012-09-27 18:42:11 -0500

    fix users/show pagination

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,5 +2,10 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find_by_username(params[:username])
     @videos = @person.videos.order("posts.created_at desc").page(params[:page]).per_page(5)
+
+    respond_to do |format|
+      format.js { render 'videos/index' }
+      format.html
+    end
   end
 end
