a73973714a6f08e15eeac39a032637a0c2a33c3d
Trevor Turk <trevorturk@gmail.com>
2012-09-27 18:26:45 -0500

    names on people/show

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,5 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find_by_username(params[:username])
     @videos = @person.videos.order("posts.created_at desc").page(params[:page]).per_page(5)
-    render 'videos/index'
   end
 end
