8d44776905f40edd23486d0c21231c970ad04d6a
Trevor Turk <trevorturk@gmail.com>
2012-09-28 14:11:28 -0500

    enable

--- lib/emailer.rb
+++ lib/emailer.rb
@@ -3,9 +3,8 @@ class Emailer
     def perform
       videos = Video.where('created_at > ?', 1.day.ago)
 
-      # Subscriber.all.each do |subscriber|
-
-        SubscriptionMailer.subscription(Subscriber.first, videos).deliver
+      Subscriber.all.each do |subscriber|
+        SubscriptionMailer.subscription(subscriber, videos).deliver
       end
     end
 end
