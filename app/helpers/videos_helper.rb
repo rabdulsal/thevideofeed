c9560c4f093bb36bf2dc27a6705e368c78169cd2
Trevor Turk <trevorturk@gmail.com>
2012-09-30 17:55:08 -0500

    temp thumbnail example

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,4 +2,8 @@ module VideosHelper
   def embed(video)
     "<iframe width='560' height='315' src='http://www.youtube.com/embed/#{video.key}' frameborder='0' allowfullscreen></iframe>".html_safe
   end
+
+  def thumbnail(video)
+    "http://i.ytimg.com/vi/#{video.key}/hqdefault.jpg"
+  end
 end
