3974e24fb150f5bfce677acced274ec6bb09b568
Trevor Turk <trevorturk@gmail.com>
2012-10-08 15:56:42 -0500

    fancybox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,10 @@
 //= require jquery
+//= require fancybox
 //= require_tree .
+
+$(document).ready(function() {
+  $("a.fancybox").fancybox({
+    'width':'9999',
+    'height':'9999'
+  });
+});
--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,13 +1,9 @@
 module VideosHelper
-  def embed(video)
-    "<iframe width='560' height='315' src='http://www.youtube.com/embed/#{video.key}' frameborder='0' allowfullscreen></iframe>".html_safe
-  end
-
-  def thumbnail(video)
+  def video_thumb(video)
     "http://i.ytimg.com/vi/#{video.key}/mqdefault.jpg"
   end
-  
-  def link(video)
+
+  def video_link(video)
     "http://www.youtube.com/embed/#{video.key}?&autoplay=1&hd=1"
   end
 end
