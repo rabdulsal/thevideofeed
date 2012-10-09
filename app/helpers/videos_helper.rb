e3eb0ec0bce2e48d53f4270cec4b5f177f33ab71
Scott McMillin <scottmcmillin@gmail.com>
2012-10-09 17:29:38 -0500

    Added vimeo to helpers; vimeo thumbnails can't be retrieved with a single request

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,13 +1,25 @@
 module VideosHelper
   def video_thumb(video)
-    "http://i.ytimg.com/vi/#{video.key}/mqdefault.jpg"
+    if video.source == "youtube"
+      "http://i.ytimg.com/vi/#{video.key}/mqdefault.jpg"
+    elsif video.source == "vimeo"
+      ""
+    end
   end
 
   def video_embed(video)
-    "http://www.youtube.com/embed/#{video.key}?&autoplay=1&hd=1"
+    if video.source == "youtube"
+      "http://www.youtube.com/embed/#{video.key}?&autoplay=1&hd=1"
+    elsif video.source == "vimeo"
+      "http://player.vimeo.com/video/#{video.key}"
+    end
   end
 
   def video_link(video)
-    "http://www.youtube.com/watch?v=#{video.key}"
+    if video.source == "youtube"
+      "http://www.youtube.com/watch?v=#{video.key}"
+    elsif video.source == "vimeo"
+      "https://vimeo.com/#{video.key}"
+    end
   end
 end
