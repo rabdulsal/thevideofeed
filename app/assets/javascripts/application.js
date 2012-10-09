d6bfa37ce674142479c01fed582dbe547585fb80
Max Mearsheimer <mmearshe@gmail.com>
2012-10-08 22:55:17 -0500

    tried to reincorporate daves changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,12 @@
 //= require fancybox
 //= require_tree .
 
-$(document).ready(function() {
-  $("a.fancybox").fancybox({
-		'width' : 853,
-    'height': 480,
-		'autoDimensions' : false,
-		'autoscale' : true,
-		'overlayOpacity' : 0.8
-  });
+
+(function($){
+	$(document).ready(function(){
+		$("a.fancybox").fancybox({
+			'width' : 853,
+	    'height': 480,
+	 	 });
+	});
 })(jQuery)
