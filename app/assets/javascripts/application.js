d76cd244bb1e4d55ac142d1783060f2cd8bd93fe
Dave Kiss <hello@davekiss.com>
2012-10-08 21:31:39 -0500

    no conflict for future js api interaction
    aka. the one thing I know how to do

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,11 @@
 //= require fancybox
 //= require_tree .
 
-$(document).ready(function() {
-  $("a.fancybox").fancybox({
-    'width':'9999',
-    'height':'9999'
+(function($){
+  $(document).ready(function(){
+    $("a.fancybox").fancybox({
+      'width':'9999',
+      'height':'9999'
+    });
   });
-});
+})(jQuery)
