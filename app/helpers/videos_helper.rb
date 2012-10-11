3c2e79767740b4b3fa317875512f302a7ae8a49a
Trevor Turk <trevorturk@gmail.com>
2012-10-11 00:09:07 -0500

    no need for the thumb helper

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,8 +1,4 @@
 module VideosHelper
-  def video_thumb(video)
-    video.thumbnail_url
-  end
-
   def video_embed(video)
     if video.source == "youtube"
       "http://www.youtube.com/embed/#{video.key}?&autoplay=1&hd=1"
