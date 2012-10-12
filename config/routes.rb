f133e820cfb9910f81ec96bf6743e13810fc9f4a
Trevor Turk <trevorturk@gmail.com>
2012-10-12 11:21:21 -0500

    nicer url for rss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Thevideofeed::Application.routes.draw do
   root :to => 'videos#index'
 
+  resources :people, only: :show
+  resources :videos, only: :index
+
   resources :subscribers, only: :create
   get 'unsubscribe/:key', to: 'subscribers#destroy', as: :unsubscribe, key: /.*/
-
-  resources :people, only: :show
 end
