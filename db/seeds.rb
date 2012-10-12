e2185e8f3efacebb00e2aba9b28a21e81e4f77b2
Trevor Turk <trevorturk@gmail.com>
2012-10-12 10:56:58 -0500

    Person.add, make setup one step

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Person.create! name: 'trevor', youtube_username: 'trevorturk', vimeo_username: 'trevorturk'
-Person.create! name: 'max', youtube_username: 'Maxami11ion'
-Person.create! name: 'scott', youtube_username: 'fotl', vimeo_username: 'scottymac'
+Person.add name: 'Trevor', youtube_username: 'trevorturk', vimeo_username: 'trevorturk'
+Person.add name: 'Max', youtube_username: 'Maxami11ion'
+Person.add name: 'Scott', youtube_username: 'fotl', vimeo_username: 'scottymac'
