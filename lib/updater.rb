946a867787a6bd042b8c276bbffbcea0d7fcc81c
Jeff Stahler <jeffreystahler@gmail.com>
2012-10-15 21:32:20 -0500

    clean up and seperate task from updater

--- lib/updater.rb
+++ lib/updater.rb
@@ -35,31 +35,6 @@ class Updater
       end
     end
 
-    def remove_videos_that_cant_be_embedded(person)
-
-      uri = URI.parse("http://gdata.youtube.com/feeds/api/users/#{person.username}/favorites?v=2")
-      response = Net::HTTP.get_response(uri).body
-      all_videos = Hash.from_xml(response)['feed']['entry'] || []    
-
-      uri = URI.parse("http://gdata.youtube.com/feeds/api/users/#{person.username}/favorites?v=2&format=5")
-      response = Net::HTTP.get_response(uri).body
-      all_videos_that_can_be_embedded = Hash.from_xml(response)['feed']['entry'] || []    
-      all_videos_that_cant_be_embedded = all_videos - all_videos_that_can_be_embedded
-      puts all_videos
-      puts all_videos_that_cant_be_embedded
-
-      all_videos_that_cant_be_embedded.each do |video|
-
-
-      person.videos(video['group']['videoid']).destroy
-      
-      end
-        
-    end
-
-
-
-
     def get_videos(person, limit, offset)
       uri = URI.parse("http://gdata.youtube.com/feeds/api/users/#{person.username}/favorites?v=2&max-results=#{limit}&start-index=#{offset}&format=5")
       response = Net::HTTP.get_response(uri).body
