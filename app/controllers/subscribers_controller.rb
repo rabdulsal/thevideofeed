4d5bd23c36fb0481c6ca932e0c1d10a9278c6051
Max Mearsheimer <mmearshe@gmail.com>
2012-10-21 22:43:16 -0500

    added red border to things

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,18 +3,18 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.new(params[:subscriber])
 
     if Subscriber.find_by_email(@subscriber.email)
-      redirect_to(root_path, :notice => "You're already signed up!")
+      redirect_to(root_path, :notice => "YOU'RE ALREADY SIGNED UP.")
     elsif @subscriber.invalid?
-      redirect_to(root_path, :notice => "The email you provided was not valid!")
+      redirect_to(root_path, :notice => "YOUR EMAIL AIN'T VALID.")
     elsif @subscriber.save
-      redirect_to(root_path, :notice => "Thanks for signing up!")
+      redirect_to(root_path, :notice => "THANKS FOR SIGNING UP.")
     else
-      redirect_to(root_path, :notice => "Something went wrong!")
+      redirect_to(root_path, :notice => "SOMETHING WENT WRONG.")
     end
   end
 
   def destroy
     Subscriber.find_by_key(params[:key]).destroy
-    render text: "You have been unsubscribed!"
+    render text: "You have been unsubscribed."
   end
 end
