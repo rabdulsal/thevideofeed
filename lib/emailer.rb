85f00609694845e24454dc586b6eedb3634fd36c
Trevor Turk <trevorturk@gmail.com>
2012-11-05 21:25:40 -0600

    Try harder to send emails for all new videos added in the last 24 hours

--- lib/emailer.rb
+++ lib/emailer.rb
@@ -1,7 +1,7 @@
 class Emailer
   class << self
     def perform
-      videos = Video.where('created_at > ?', 1.day.ago).includes(:first_person)
+      videos = Video.where('created_at > ?', 24.hours.ago.utc).includes(:first_person)
 
       if videos.any?
         Subscriber.all.each do |subscriber|
