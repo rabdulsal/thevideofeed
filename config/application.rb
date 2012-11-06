9136b059031af89f0dda1475eaa21d14e1c08014
Trevor Turk <trevorturk@gmail.com>
2012-11-05 21:33:59 -0600

    make sure we use utc

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ end
 
 module Thevideofeed
   class Application < Rails::Application
+    config.time_zone = "UTC"
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
