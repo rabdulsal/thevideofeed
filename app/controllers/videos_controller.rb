2c193fedf153089785fabfac1a5c096976152de9
Rashad Abdul-Salaam <rashad@getskinclick.com>
2014-05-19 13:50:52 -0500

    Added JSON render to Videos Controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,10 @@
 class VideosController < ApplicationController
   def index
     @videos = Video.order("created_at desc").includes(:first_person).page(params[:page]).per_page(Video::PER_PAGE)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @videos }
+    end
   end
 end
